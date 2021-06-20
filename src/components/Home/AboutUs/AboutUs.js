95524dda8595a25b3aef6ad6f0d6cf975c4575eb
prashant kumar <prashantju2@gmail.com>
2021-06-20 22:27:30 +0530

    padding in about us content and button added to read more

--- src/components/Home/AboutUs/AboutUs.js
+++ src/components/Home/AboutUs/AboutUs.js
@@ -1,29 +1,24 @@
 import React from 'react';
 import './AboutUs.css';
+import { Navbar, Nav } from "react-bootstrap";
 
 function AboutUs()
 {
   return(
-    <>
+   <>
       <div className="home_aboutus">
+        <br/><br/>
          <h1 className="heading">WHAT IS JUMHC ?  </h1>
-        
-         <div className="sp">
-
-            <p>This is one of the oldest student run Mountaineering and Hiking Club in India,      
-                Jadavpur University Mountaineering and Hiking Club (JUMHC)       
-              </p>
-
-              <p>is one of the official club and societies of Jadavpur University. Founded in 1966 under the initiative of *******************and associates . . . .</p>                                            
-                           
-
-            <p>Our two major events are <strong>RCC</strong> and <strong>Expedition</strong></p>
-         </div>
-         <div>
-          <a href="#">read more</a>
-          </div>
-      </div>
-    </>
+           <div className="para">
+             <p>This is one of the oldest student run Mountaineering and Hiking Club in India,      
+                Jadavpur University Mountaineering and Hiking Club (JUMHC)</p>  
+                <p>is one of the official club and societies of Jadavpur University. Founded in 1966 under the initiative of *******************and associates . . . . .</p>                                            
+              <p>Our two major events are <strong>RCC</strong> and <strong>Expedition</strong></p>
+            </div>
+            <button>read more</button>
+       </div>
+     
+   </>
   );
 }
 export default AboutUs;
