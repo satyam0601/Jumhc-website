531d9a30b1e8d79aaef7efddc75cec3c4ab4128c
prashant kumar <prashantju2@gmail.com>
2021-06-21 20:57:35 +0530

    aboutus fixed and text-align is remains center

--- src/components/Home/AboutUs/AboutUs.js
+++ src/components/Home/AboutUs/AboutUs.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import './AboutUs.css';
-import { Navbar, Nav } from "react-bootstrap";
-
-function AboutUs()
-{
-  return(
-   <>
-      <div className="home_aboutus">
-        <br/><br/>
-         <h1 className="heading">WHAT IS JUMHC ?  </h1>
-           <div className="para">
-             <p>This is one of the oldest student run Mountaineering and Hiking Club in India,      
-                Jadavpur University Mountaineering and Hiking Club (JUMHC)</p>  
-                <p>is one of the official club and societies of Jadavpur University. Founded in 1966 under the initiative of *******************and associates . . . . .</p>                                            
-              <p>Our two major events are <strong>RCC</strong> and <strong>Expedition</strong></p>
-            </div>
-            <button>read more</button>
-       </div>
-     
-   </>
-=======
 import React from "react";
 import "./AboutUs.css";
 import Button from "../../Common/Button/Button";
@@ -59,8 +36,6 @@ function AboutUs() {
           <Button href="#about-us">Read More</Button>
         </div>
       </div>
-    </>
->>>>>>> 170054472962fe15d33b1ac538275ad1b9bbf0a7
-  );
+    </>);
 }
 export default AboutUs;
