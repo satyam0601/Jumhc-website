99f0a0005b0b696a4b669f506913e2ec4ea9b262
Satyam Gupta <satyam0601@gmail.com>
2021-06-24 01:54:36 +0530

    feature contact us added responsive

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import Home from "./components/Home/Home";
 import Footer from "./components/Footer/Footer";
 import Expedition from "./components/Expedition/Expedition";
 import React from "react";
+import ContactUs from "./components/ContactUs/ContactUs";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <NavigationBar />
+      <ContactUs />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
