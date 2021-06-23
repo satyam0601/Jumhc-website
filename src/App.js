4a0f33badfc70c5f76ecbb8205ce8d5c972024fb
Satyam Gupta <satyam0601@gmail.com>
2021-06-23 12:04:03 +0530

    feature-blog-card-added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import NavigationBar from "./components/NavigationBar/NavigationBar";
 import Home from "./components/Home/Home";
 import Footer from "./components/Footer/Footer";
 import React from "react";
-
+import Expedition from "./components/Expedition/Expedition";
 
 function App() {
   return (
     <>
       <NavigationBar />
-      <Home />
+      <Home/>
       <Footer />
     </>
   );
