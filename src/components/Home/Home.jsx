232761b58c211d91661fd344b57a197596b700dc
Satyam Gupta <satyam0601@gmail.com>
2021-06-23 23:16:04 +0530

    Home-page-css-removed

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import AboutUs from "./AboutUs/AboutUs";
 import ContentContainer from "../Common/ContentContainer/ContentContainer.jsx";
 import Slider from "./Slider/Slider";
-import "./Home.css";
 function Home() {
   return (
-    <ContentContainer>
+    <>
       <Slider />
-      <AboutUs />
-    </ContentContainer>
+      <ContentContainer>
+        <AboutUs />
+      </ContentContainer>
+    </>
   );
 }
 
--- src/components/Home/Slider/Slider.jsx
+++ src/components/Home/Slider/Slider.jsx
@@ -4,39 +4,60 @@ import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
 import "swiper/swiper.min.css";
-import "swiper/components/navigation/navigation.min.css";
 import "swiper/components/pagination/pagination.min.css";
+import "swiper/components/navigation/navigation.min.css";
 
 import "./Slider.css";
 
 // import Swiper core and required modules
-import SwiperCore, {
-  Navigation,
-  Pagination,
-  Mousewheel,
-  Keyboard,
-} from "swiper/core";
+import SwiperCore, { Pagination, Navigation } from "swiper/core";
 
 // install Swiper modules
-SwiperCore.use([Navigation, Pagination, Mousewheel, Keyboard]);
+SwiperCore.use([Pagination, Navigation]);
 
 export default function App() {
   return (
     <>
       <Swiper
-        cssMode={true}
-        navigation={true}
-        pagination={true}
-        mousewheel={true}
-        keyboard={true}
+        slidesPerView={1}
+        spaceBetween={30}
         loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
         className="mySwiper"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="https://images.unsplash.com/photo-1458442310124-dde6edb43d10?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
+            alt=""
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="https://images.unsplash.com/photo-1522163182402-834f871fd851?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1893&q=80"
+            alt=""
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="https://images.unsplash.com/photo-1624298574806-019c81c19244?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
+            alt=""
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="https://images.unsplash.com/photo-1624347616827-ea7fb9a3cfbb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
+            alt=""
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="https://images.unsplash.com/photo-1502126324834-38f8e02d7160?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1950&q=80"
+            alt=""
+          />
+        </SwiperSlide>
       </Swiper>
     </>
   );
