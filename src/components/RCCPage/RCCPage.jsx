d2806ca788ccbf7857cdfc81e9bc9387471b952a
Ehtesham <ehteshamkhan3014@gmail.com>
2021-06-24 14:36:18 +0530

    RCCPage

---
+++ src/components/RCCPage/RCCPage.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import ContentContainer from "../Common/ContentContainer/ContentContainer";
+import "./RCCPage.css";
+
+function RCCPage() {
+  return (
+    <ContentContainer>
+      <div className="rcc-page-container">
+        <h1 className="rcc-page-heading">RCC</h1>
+        <div className="rcc-content">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est sdhdfg yu hjdjgfy
+          </p>
+        </div>
+        <div className="rcc-image-container">
+          <img src="#photo" width="950" height="700"></img>
+        </div>
+        <div className="rcc-content">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est sdhdfg yu hjdjgfy
+          </p>
+        </div>
+        <div className="rcc-image-container">
+          <img src="#photo" width="950" height="700"></img>
+        </div>
+        <div className="rcc-content">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est sdhdfg yu hjdjgfy
+          </p>
+        </div>
+      </div>
+    </ContentContainer>
+  );
+}
+export default RCCPage;
