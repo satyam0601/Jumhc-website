09aa9350a6f8d974efb93a620b19c9dd9acee376
Ehtesham <ehteshamkhan3014@gmail.com>
2021-06-25 19:58:59 +0530

    BlogPage fixed

--- src/components/BlogPage/BlogPage.jsx
+++ src/components/BlogPage/BlogPage.jsx
@@ -6,10 +6,8 @@ function BlogPage(props) {
     <div className="blog-page-container">
       <h1 className="heading">{props.children[0]}</h1>
       <div className="image-container">{props.children[1]}</div>
-      <div className="blog-paragraph">{props.children[2]}</div>
-      <div className="author-name-container">
-        <span className="author-name">{props.children[3]}</span>
-      </div>
+      <p className="blog-paragraph">{props.children[2]}</p>
+      <span className="author-name">{props.children[3]}</span>
     </div>
   );
 }
