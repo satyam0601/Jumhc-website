d7f55e1b45406f0b254f7514caabad9d6aa704b9
satyam0601 <54521651+satyam0601@users.noreply.github.com>
2021-06-25 13:53:24 +0530

    outdoor indoor fix

--- src/App.js
+++ src/App.js
@@ -7,14 +7,12 @@ import React from "react";
 import RCCPage from "./components/RCCPage/RCCPage";
 import ContactUs from "./components/ContactUs/ContactUs";
 import Trek from "./components/Trek/Trek";
-import OutdoorIndoor from './components/OutdoorIndoor/OutdoorIndoor';
 
 function App() {
   return (
     <>
       <NavigationBar />
-      <Trek />
-      <Home/>
+      <Home />
       <Footer />
     </>
   );
---
+++ src/components/Home/OutdoorIndoor/OutdoorIndoor.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+import "./OutdoorIndoor.css";
+import ContentContainer from "../../Common/ContentContainer/ContentContainer";
+function OutdoorIndoor() {
+  return (
+    <>
+      <ContentContainer>
+        <div className="pagecontent">
+          <div className="indore">
+            <h1>this is indoor content</h1>
+            <img
+              className="indoorimg"
+              src="https://images.unsplash.com/photo-1458442310124-dde6edb43d10?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
+            />
+            <p className="indoorcontent">
+              "On the other hand, we denounce with righteous indignation and
+              dislike men who are so beguiled and demoralized by the charms of
+              pleasure of the moment, so blinded by desire, that they cannot
+              foresee the pain and trouble that are bound to ensue; and equal
+              blame belongs to those who fail in their duty through weakness of
+              will, which is the same as saying through shrinking from toil and
+              pain. These cases are perfectly simple and easy to distinguish. In
+              a free hour, when our power of choice is untrammelled and when
+              nothing prevents our being able to do what we like best, every
+              pleasure is to be welcomed and every pain avoided. But in certain
+              circumstances and owing to the claims of duty or the obligations
+              of business it will frequently occur that pleasures have to be
+              repudiated and annoyances accepted. The wise man therefore always
+              holds in these matter to this principle of selection: he rejects
+              pleasures to secure other greater pleasures, or else he endures
+              pains to avoid worse pains."
+            </p>
+          </div>
+          <div className="outdoor">
+            <h1>this is outdoor content</h1>
+            <img
+              className="outdoorimg"
+              src="https://images.unsplash.com/photo-1458442310124-dde6edb43d10?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
+            />
+            <p className="indoorcontent">
+              "On the other hand, we denounce with righteous indignation and
+              dislike men who are so beguiled and demoralized by the charms of
+              pleasure of the moment, so blinded by desire, that they cannot
+              foresee the pain and trouble that are bound to ensue; and equal
+              blame belongs to those who fail in their duty through weakness of
+              will, which is the same as saying through shrinking from toil and
+              pain. These cases are perfectly simple and easy to distinguish. In
+              a free hour, when our power of choice is untrammelled and when
+              nothing prevents our being able to do what we like best, every
+              pleasure is to be welcomed and every pain avoided. But in certain
+              circumstances and owing to the claims of duty or the obligations
+              of business it will frequently occur that pleasures have to be
+              repudiated and annoyances accepted. The wise man therefore always
+              holds in these matter to this principle of selection: he rejects
+              pleasures to secure other greater pleasures, or else he endures
+              pains to avoid worse pains."
+            </p>
+          </div>
+          <div className="natural">
+            <h1>this is natural content</h1>
+            <img
+              className="naturalimg"
+              src="https://images.unsplash.com/photo-1458442310124-dde6edb43d10?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
+            />
+            <p className="indoorcontent">
+              "On the other hand, we denounce with righteous indignation and
+              dislike men who are so beguiled and demoralized by the charms of
+              pleasure of the moment, so blinded by desire, that they cannot
+              foresee the pain and trouble that are bound to ensue; and equal
+              blame belongs to those who fail in their duty through weakness of
+              will, which is the same as saying through shrinking from toil and
+              pain. These cases are perfectly simple and easy to distinguish. In
+              a free hour, when our power of choice is untrammelled and when
+              nothing prevents our being able to do what we like best, every
+              pleasure is to be welcomed and every pain avoided. But in certain
+              circumstances and owing to the claims of duty or the obligations
+              of business it will frequently occur that pleasures have to be
+              repudiated and annoyances accepted. The wise man therefore always
+              holds in these matter to this principle of selection: he rejects
+              pleasures to secure other greater pleasures, or else he endures
+              pains to avoid worse pains."
+            </p>
+          </div>
+        </div>
+      </ContentContainer>
+    </>
+  );
+}
+
+export default OutdoorIndoor;
