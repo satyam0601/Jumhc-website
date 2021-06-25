f996a6563eafae390f13fbe6c55f95b34e50d27f
Ehtesham <ehteshamkhan3014@gmail.com>
2021-06-25 15:38:51 +0530

    Removed redundant div from BlogPage

--- src/components/BlogPage/BlogPage.jsx
+++ src/components/BlogPage/BlogPage.jsx
@@ -4,7 +4,7 @@ import "./BlogPage.css";
 function BlogPage(props) {
   return (
     <div className="blog-page-container">
-      <div className="heading">{props.children[0]}</div>
+      <h1 className="heading">{props.children[0]}</h1>
       <div className="image-container">{props.children[1]}</div>
       <div className="blog-paragraph">{props.children[2]}</div>
       <div className="author-name-container">
