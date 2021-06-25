b59ae7ad207c48ddd2ab5103803cdbe4aa8867cf
prashant kumar <prashantju2@gmail.com>
2021-06-25 14:38:53 +0530

    outdoorindoor module is added to home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import AboutUs from "./AboutUs/AboutUs";
 import ContentContainer from "../Common/ContentContainer/ContentContainer.jsx";
 import Slider from "./Slider/Slider";
-import OutdoorIndoor from './components/OutdoorIndoor/OutdoorIndoor';
+import OutdoorIndoor from '../OutdoorIndoor/OutdoorIndoor';
 function Home() {
   return (
     <>
